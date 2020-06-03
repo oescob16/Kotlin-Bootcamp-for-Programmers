2cda58b98d84a9cc97a27bbbc954596c9626cc89
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-06-02 19:58:39 -0600

    Added Init Blocks and a Secondary Constructor in the Aquarium class

--- src/example/myapp/Aquarium.kt
+++ src/example/myapp/Aquarium.kt
@@ -1,13 +1,19 @@
 package example.myapp
 
-class Aquarium {
-    var height: Int = 40
-    var width: Int = 20
-    var length: Int = 100
-
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init {
+        println("Aquarium Initializing")
+    }
+    init {
+        println("Volume: ${width * length * height / 1000} l")
+    }
+    constructor(numberOfFish: Int): this(){
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
     fun printSize(){
-        println("Width: $width")
-        println("Heihgt: $height")
-        println("Length: $length" + "\n")
+        println("Width: $width cm")
+        println("Height: $height cm")
+        println("Length: $length cm" + "\n")
     }
 }
--- src/example/myapp/main.kt
+++ src/example/myapp/main.kt
@@ -1,8 +1,21 @@
 package example.myapp
 
 fun buildAquarium(){
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
+
+    val aquarium5 = Aquarium(numberOfFish = 29)
+    aquarium5.printSize()
+    println("Volume: ${aquarium5.width *aquarium5.length * aquarium5.height / 1000} l")
 }
 
 fun main() {
